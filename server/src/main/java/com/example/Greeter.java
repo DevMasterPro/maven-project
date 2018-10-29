e774bc7d92abcdf8d7ee70f3208c145e83959b78
Shahin Ahmed <shahinahmed100@hotmail.com>
2018-10-29 12:31:15 +0000

    modified the check style warning

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,14 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+/**
+
+ * @PARAM someone the name of a person 
+ * @RETURN  The Greeting String
+ 
+ */
+
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
